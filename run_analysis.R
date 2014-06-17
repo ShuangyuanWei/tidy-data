13617fe6eb8dede8e83f25dccdb86c5f6d1d20c6
Yuri Chemolosov <yuri.chemolosov@radialpoint.com>
2014-06-17 17:45:12 -0400

    decoded activity column added only 'std' and 'mean' values remaining

--- run_analysis.R
+++ run_analysis.R
@@ -1,22 +1,32 @@
 library("data.table")
 
 read_dataset <- function (folder="UCI HAR Dataset") {
-    labels = fread(paste(folder, "features.txt",sep="\\"))
+    labels <- fread(paste(folder, "features.txt",sep="\\"))
     
-    sfile = paste(folder, "test", "subject_test.txt",sep="\\")
-    subject = fread(sfile)
-    
-    lfile = paste(folder, "test", "y_test.txt",sep="\\")
-    activity = fread(lfile)
+    sfile <- paste(folder, "test", "subject_test.txt",sep="\\")
+    subject <- fread(sfile)
     
     #open the measurement dataset and assign column lables
     #convert it to data.table (fread fails reading source file)
-    dfile = paste(folder, "test", "X_test.sample.txt",sep="\\")
-    data <- as.data.table(read.table(dfile, col.names=labels[,V2]))
-
+    rowdata = read.table(paste(folder, "test", "X_test.sample.txt",sep="\\"), 
+                         col.names=labels$V2)
+    #converting data.table, leave only 'std' and 'mean' calculated columns
+    # all the columns containing "-std()" or "-mean()" string in the label
+    data <- as.data.table(rowdata[,grep("-(std|mean)\\(\\)", labels$V2)])
+    
     #add activity column to the dataaset
-    data[,activity:=activity[[1]]]
+    data[,activity:=get_activities(folder)]
     #replaces activity factor code by descriptive name
     #todo
-    head(data,1)
+    head(data)
+}
+
+get_activities <- function(folder="UCI HAR Dataset") {
+    actlabels <- fread(paste(folder, "activity_labels.txt",sep="\\"))
+    
+    actdata <- fread(paste(folder, "test", "y_test.txt",sep="\\"))
+    
+    #create factor translating activity code to descriptive names
+    factor(actdata$V1, levels=actlabels$V1, labels=actlabels$V2)
+    
 }
