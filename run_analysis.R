b573da8f150626270e6515814e76938e7f4a18d9
Yuri Chemolosov <yuri.chemolosov@radialpoint.com>
2014-06-18 16:57:08 -0400

    combines datasets, adds activity and subject columns #1-#3

--- run_analysis.R
+++ run_analysis.R
@@ -1,32 +1,47 @@
 library("data.table")
 
-read_dataset <- function (folder="UCI HAR Dataset") {
-    labels <- fread(paste(folder, "features.txt",sep="\\"))
+tidy_set <- function(folder="UCI HAR Dataset") {
+
+    labels <- fread(paste(folder, "features.txt",sep="/"))
+
+    read_dataset <- function (type) {
+        #type may be 'train' or 'test', it is a folder name part of file name
+        #load the measurement dataset and assign column lables
+        fileName <- paste(folder, type, paste("X_", type, ".sample.txt", sep=""), sep="/")
+        rowdata <- data.table(read.table(fileName))
+        
+        tidy <- as.data.table(rowdata[,grep("-(std|mean)\\(\\)", labels$V2)])
+
+        #add subject column
+        fileName <- paste(folder, type, paste("subject_", type, ".sample.txt", sep=""), sep="/")
+        subject <- fread(fileName)
+        
+        tidy[,subject:=subject$V1]
+        
+        #add activity column to the dataset 
+        #replacing activity code with its descriptive name
+        fileName <- paste(folder, type, paste("y_", type, ".sample.txt", sep=""), sep="/")
+        activity <- fread(fileName)
+        
+        tidy[,activity:=activity$V1]
+        
+        tidy
+        
+    }
     
-    sfile <- paste(folder, "test", "subject_test.txt",sep="\\")
-    subject <- fread(sfile)
-    
-    #open the measurement dataset and assign column lables
-    #convert it to data.table (fread fails reading source file)
-    rowdata = read.table(paste(folder, "test", "X_test.sample.txt",sep="\\"), 
-                         col.names=labels$V2)
-    #converting data.table, leave only 'std' and 'mean' calculated columns
+    #read and combine two datasets
+    data <- rbind(read_dataset("test"), read_dataset("train"))
+
+    #convert to data.table, leave only 'std' and 'mean' calculated columns
     # all the columns containing "-std()" or "-mean()" string in the label
-    data <- as.data.table(rowdata[,grep("-(std|mean)\\(\\)", labels$V2)])
+    #data <- as.data.table(rowdata[,grep("-(std|mean)\\(\\)", labels$V2)])
     
-    #add activity column to the dataaset
-    data[,activity:=get_activities(folder)]
-    #replaces activity factor code by descriptive name
-    #todo
-    head(data)
+    
+    actlabels <- fread(paste(folder, "activity_labels.txt",sep="/"))
+    data[,activity:=factor(data$activity, 
+                           levels=actlabels$V1, 
+                           labels=actlabels$V2)]
+    
+    data
 }
 
-get_activities <- function(folder="UCI HAR Dataset") {
-    actlabels <- fread(paste(folder, "activity_labels.txt",sep="\\"))
-    
-    actdata <- fread(paste(folder, "test", "y_test.txt",sep="\\"))
-    
-    #create factor translating activity code to descriptive names
-    factor(actdata$V1, levels=actlabels$V1, labels=actlabels$V2)
-    
-}
