001105b52181e9c153989dc15b5e124a7d467fca
Yuri Chemolosov <yuri.chemolosov@radialpoint.com>
2014-06-20 11:23:56 -0400

    comments added to the code, read me to explain using the script

--- README.md
+++ README.md
@@ -1,4 +1,47 @@
 tidy-data
 =========
 
+## Summary
 Course project for Coursera Getting and Cleaning Data online course
+
+The `run_analysis.R` script processes data set available for download from
+   (https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip) 
+
+as it is required by course project requirements:
+1. Merges the training and the test sets to create one data set.
+2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+3. Uses descriptive activity names to name the activities in the data set
+4. Appropriately labels the data set with descriptive variable names. 
+5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject.  
+
+## Script
+#### dependencies
+The script will require `data.table` package which may be installed by 
+```
+install.packages("data.table")
+```
+
+#### running
+Main script to run is `run_analysis.R` it is expecting a parameter with the folder where raw data had been unpacked to.
+If input parameter is omitted, the script will look for the unzipped data in its source folder.
+Unziping data into script's folder will create sub-folder named `UCI HAR Dataset` where the script will read data from.
+
+Script could be run by issuing
+```
+data <- tidy_set()
+```
+
+or to explicitly specify data folder
+
+```
+data <- tidy_set('/tmp/data/UCI HAR Dataset')
+```
+
+#### output
+Returned object is `data.table` contains `180` rows and `68` columns.
+Resulting data saved into tidy_data.txt file by 
+```
+write.table(data, file="tidy_data.txt", row.names=FALSE)
+```
+
+variable names and values are described in details in `CodeBook.md`
--- run_analysis.R
+++ run_analysis.R
@@ -1,20 +1,39 @@
+# Coursera 'Getting and Cleanind Data' course project
+# Ths script is transforming dataset available from 
+#   https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+# dataset must be downloaded and unpacked into the local folder
 library("data.table")
 
 tidy_set <- function(folder="UCI HAR Dataset") {
-
-    labels <- read.table(paste(folder, "features.txt",sep="/"), stringsAsFactors=FALSE)
-    columns <- grep("-(std|mean)\\(\\)", labels$V2)
+    # Args:
+    #   folder: folder path for source data. Ommiting this parametere will
+    #     make script look up in ./UCI HAR Dataset as it has been created
+    #     unzipping original data in the script's current folder
+    #
+    # Returns:
+    #   Tidy data set as requested by course project requirements
+    #   se README.md for details
     
-    tidy_labels <- gsub("\\(|\\)","", gsub("-", ".", labels[columns, 2]))
+    # load data labels wich are common for two data sets
+    labels <- read.table(paste(folder, "features.txt",sep="/"), 
+                         stringsAsFactors=FALSE)
+    
+    # create columnn list and tidy labels for data filtering and labeling
+    columns <- grep("-(std|mean)\\(\\)", labels$V2)
+    tidy_labels <- gsub("\\(|\\)","", gsub("-", "_", labels[columns, 2]))
 
+    #internal function loading and transforming source data into data.table
+    #loads files from folder specified by folder variable
     read_dataset <- function (type) {
-        #type may be 'train' or 'test', it is a folder name part of file name
-        #load the measurement dataset and assign column lables
+        # Args: 
+        #    type: may be 'train' or 'test', it is used to select proper folder
+        #          and as part of file name
         fileName <- paste(folder, type, paste("X_", type, ".txt", sep=""), sep="/")
         rowdata <- read.table(fileName)
         
+        #only leave relevant columns (step #2)
         tidy <- rowdata[,columns]
-
+        #set descriptinve names to columns (step #3)
         setnames(tidy, tidy_labels) 
         
         #add subject column
@@ -23,31 +42,30 @@ tidy_set <- function(folder="UCI HAR Dataset") {
         
         tidy$subject <- subject$V1
         
-        #add activity column to the dataset 
-        #replacing activity code with its descriptive name
+        #add activity column  
         fileName <- paste(folder, type, paste("y_", type, ".txt", sep=""), sep="/")
         activity <- fread(fileName)
         
         tidy$activity <- activity$V1
         
+        #transform data.frame into data.table facilitating following data merge
+        #and transformation
         as.data.table(tidy)
         
     }
     
-    #read and combine two datasets
+    #read and combine two datasets, resulting object is data table
+    #it has all thec colunns properly named and rows sequentially numbered
     data <- rbind(read_dataset("test"), read_dataset("train"))
 
-    #convert to data.table, leave only 'std' and 'mean' calculated columns
-    # all the columns containing "-std()" or "-mean()" string in the label
-
-    
+    #replace activity codes with descriptive names
     actlabels <- fread(paste(folder, "activity_labels.txt",sep="/"))
     data[,activity:=factor(data$activity, 
                            levels=actlabels$V1, 
                            labels=actlabels$V2)]
     
-    #last manipulataion on data to find average of all the columns
-    #grouped by subject and activity fields
+    # last manipulataion on data to find average values of all the columns
+    # grouped by subject and activity fields. 
+    # Returning resulting data table 
     data[,lapply(.SD,mean),by=c("activity","subject")]
 }
-
